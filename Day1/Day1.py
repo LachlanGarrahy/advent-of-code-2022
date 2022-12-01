9d1f29d4a51b842c7a21dd74af67d69c34c94c63
OILSLICC <LachiKent@gmail.com>
2022-12-02 07:27:50 +1000

    Day1

---
+++ Day1/Day1.py
@@ -0,0 +1,29 @@
+
+def GetCalories():
+    with open("input.txt") as f:
+        lines = f.readlines()
+        calories = []
+        currentCalories = 0
+        for i in lines:
+            if i == "\n":
+                calories.append(currentCalories)
+                currentCalories = 0
+            else:
+                currentCalories += int(i)
+        return(calories)
+        
+
+def SortList(calories):
+    calories.sort(reverse=True)
+    return calories
+
+def addTopThree(calories):
+    return calories[0] + calories[1] + calories[2]
+
+
+
+calories = GetCalories()
+sortedCalories = SortList(calories)
+totalTop3 = addTopThree(sortedCalories)
+print(totalTop3)
+
