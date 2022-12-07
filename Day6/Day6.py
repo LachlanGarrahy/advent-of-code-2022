4dd310e5756101ef64ac11869cd78aed5efc7251
OILSLICC <LachiKent@gmail.com>
2022-12-07 16:49:30 +1000

    Day6 clean

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,16 +1,15 @@
 def day6(distinctChar):
     charList = []
-    counter = 0
-    for l in open('input.txt'):
-        for char in l:
-            counter += 1
-            charList.append(char)
+    with open("input.txt") as f:
+        line = f.readlines()[0]
+        for char in range(len(line)):
+            charList.append(line[char])
             if(len(charList) != len(set(charList))):
                 charList.pop(0)
                 continue
             if(len(charList) == distinctChar):
+                print(char+1)
                 break
-    print(counter)
 
 day6(4)
 day6(14)
